66cc3b7d87686cd7b34a6b78209f3383f69b38f7
Rylan Dmello <rylandmello@gmail.com>
2019-10-27 17:23:07 -0400

    Experimenting with mousekeys settings

--- keyboards/keebio/levinson/keymaps/rdmello/config.h
+++ keyboards/keebio/levinson/keymaps/rdmello/config.h
@@ -29,3 +29,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Rylan, trying more MODTAP settings
 #define PERMISSIVE_HOLD
+
+/* Rylan's mousekeys overrides
+* I prefer faster movement of the mouse here, */
+// MOUSEKEY_DELAY defaults to 300, but let's try a quicker one.
+#define MOUSEKEY_DELAY 50
+
+// MOUSEKEY_INTERVAL defaults to 50, 25 should make it a bit smoother.
+#define MOUSEKEY_INTERVAL 35
+
+// This is like constant speed movement.
+#define MOUSEKEY_TIME_TO_MAX 5
+
+//
+#define MOUSEKEY_MAX_SPEED 10
--- keyboards/keebio/levinson/keymaps/rdmello/keymap.c
+++ keyboards/keebio/levinson/keymaps/rdmello/keymap.c
@@ -30,7 +30,6 @@ enum custom_keycodes {
    ADJUST, RAISE, LOWER, KC_LGUI, LOWER, LGUI_T(KC_SPC),      KC_LCTL, RAISE, KC_LALT, _______, _______, _______ \
 )
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
@@ -61,16 +60,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] =  RYLAN_LAYOUT( \
-  KC_TILD,        KC_BTN2, KC_MS_U, KC_BTN1, KC_MS_WH_DOWN, KC_HOME,      KC_GRV, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_DEL, \
-  CTL_T(KC_ESC),  KC_MS_L, KC_MS_D, KC_MS_R,   KC_MS_WH_UP,  KC_END,     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BSLS, KC_LOCK, \
-  KC_LSFT,           KC_1,    KC_2,    KC_3,          KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_ENT  \
+  KC_TILD,        KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, KC_HOME,      KC_GRV, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_DEL, \
+  CTL_T(KC_ESC),  KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,  KC_END,     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BSLS, KC_LOCK, \
+  KC_LSFT,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_ENT  \
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   =  |   1  |   2  |   3  |   4  |   5  |  ~   |  _   |  +   |   {  |   }  |      |
+ * |      |M-BTN2|M-SRUP|M-BTN1|      |      |  ~   |  _   |  +   |   {  |   }  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |   .  |   0  |   9  |   8  |   7  |   6  |FFXBCK| PGDN | PGUP |FFXFWD|   |  |      |
+ * |      |M-SRLT|M-SRDN|M-SRRT|      |      |FFXBCK| PGDN | PGUP |FFXFWD|   |  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |  ~   |   !  |   @  |   #  |   $  |   %  |  ^   |  &   |   *  |  (   |   )  |   |  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -78,26 +77,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = RYLAN_LAYOUT( \
-  KC_EQL,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_TILD,    KC_UNDS, KC_PLUS,    KC_LCBR, KC_RCBR, _______, \
-  KC_DOT,     KC_0,    KC_9,    KC_8,    KC_7,    KC_6,     G(KC_LEFT), KC_PGDN, KC_PGUP, G(KC_RGHT), KC_PIPE, _______, \
-  KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,     KC_CIRC,    KC_AMPR, KC_ASTR,    KC_LPRN, KC_RPRN, KC_PIPE  \
+  _______, KC_BTN2, KC_WH_U, KC_BTN1, _______, _______,  KC_TILD,    KC_UNDS, KC_PLUS,    KC_LCBR, KC_RCBR, _______, \
+  _______, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______,  G(KC_LEFT), KC_PGDN, KC_PGUP, G(KC_RGHT), KC_PIPE, _______, \
+  KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,  KC_CIRC,    KC_AMPR, KC_ASTR,    KC_LPRN, KC_RPRN, KC_PIPE  \
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      | MUTE | VOL+ |      |      |      |      |      |      |      |      |      |
+ * |M-SPD2| MUTE | VOL+ |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      | BRI- | VOL- | BRI+ |      |      |      |      |      |      |      | F12  |
+ * |M-SPD1| BRI- | VOL- | BRI+ |      |      |      |      |      |      |      | F12  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |M-SPD0|  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      | TGL  | TGL  |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_ortho_4x12( \
-  _______, KC_MUTE, KC_VOLU, _______, _______, _______,       _______, _______, _______, _______, _______, _______, \
-  _______, KC_BRID, KC_VOLD, KC_BRIU, _______, _______,       _______, _______, _______, _______, _______,  KC_F12, \
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, \
+  KC_ACL2, KC_MUTE, KC_VOLU, _______, _______, _______,       _______, _______, _______, _______, _______, _______, \
+  KC_ACL1, KC_BRID, KC_VOLD, KC_BRIU, _______, _______,       _______, _______, _______, _______, _______,  KC_F12, \
+  KC_ACL0,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, \
   _______, _______, _______, _______, _______, MAGIC_TOGGLE_CTL_GUI,       MAGIC_TOGGLE_CTL_GUI, _______, _______, _______, _______, _______  \
 )
 };
