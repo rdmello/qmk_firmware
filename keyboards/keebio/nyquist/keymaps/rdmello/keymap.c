8676b237ff85e6f77f717b6652d494437e1af2d6
Rylan Dmello <rylandmello@gmail.com>
2019-11-15 18:31:25 -0500

    Adding basic game layer for Nyquist

--- keyboards/keebio/nyquist/keymaps/rdmello/keymap.c
+++ keyboards/keebio/nyquist/keymaps/rdmello/keymap.c
@@ -11,6 +11,8 @@ extern keymap_config_t keymap_config;
 #define _DVORAK 2
 #define _LOWER 3
 #define _RAISE 4
+#define _GAME 5
+#define _GAMEMOD 6
 #define _ADJUST 16
 
 enum custom_keycodes {
@@ -18,6 +20,8 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
+  GAME,
+  GAMEMOD,
 };
 
 /* COMMON ROW
@@ -27,7 +31,7 @@ enum custom_keycodes {
  */
 #define RYLAN_LAYOUT(...) LAYOUT_ortho_5x12( \
    __VA_ARGS__, \
-   ADJUST, RAISE, LOWER, KC_LCTL, LOWER, KC_LGUI,      KC_SPC, RAISE, KC_LCTL, KC_LALT, RAISE, _______ \
+   ADJUST, RAISE, LOWER, KC_LCTL, LOWER, GUI_T(KC_SPC),      KC_SPC, RAISE, KC_LALT, KC_LCTL, RAISE, _______ \
 )
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -48,6 +52,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,       KC_I,    KC_O,    KC_P, KC_BSPC,   \
   CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,       KC_K,    KC_L, KC_SCLN, KC_QUOT,   \
   KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH, KC_SFTENT  \
+),
+
+/*
+ * Game - This layer is like QWERTY but with some slight modifications.
+ */
+[_GAME] = LAYOUT_ortho_5x12( \
+  _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   \
+  KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,       KC_I,    KC_O,    KC_P, KC_BSPC,   \
+  CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,       KC_K,    KC_L, KC_SCLN, KC_QUOT,   \
+  KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH, KC_SFTENT, \
+  ADJUST, RAISE, LOWER, KC_LCTL, GAMEMOD, KC_ENT,      KC_SPC, LOWER, KC_LCTL, KC_LALT, RAISE, _______
+),
+
+/*
+ * GameModifiers - This layer is like LOWER but with some slight modifications.
+ */
+[_GAMEMOD] = LAYOUT_ortho_5x12( \
+  _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, \
+  KC_TAB,  _______, KC_UP,   _______, _______, QWERTY,   _______, _______, _______, _______, _______, _______, \
+  KC_ESC,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,  _______, _______, _______, _______, _______, _______, \
+  KC_LSFT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_ENT,  \
+  ADJUST, RAISE, LOWER, KC_LCTL, GAMEMOD, KC_ENT,         KC_SPC, LOWER, KC_LCTL, KC_LALT, RAISE, _______ 
 ),
 
 /* Raise
@@ -72,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |M-BTN2|M-SRUP|M-BTN1|      |      |  ~   |  _   |  +   |   {  |   }  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |M-SRLT|M-SRDN|M-SRRT|      |      |FFXBCK|M-SRDN|M-SRUP|FFXFWD|   |  |      |
+ * |      |M-SRLT|M-SRDN|M-SRRT|      |      |FFXBCK| PGDN | PGUP |FFXFWD|   |  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |  ~   |   !  |   @  |   #  |   $  |   %  |  ^   |  &   |   *  |  (   |   )  |   |  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -82,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = RYLAN_LAYOUT( \
   _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
   _______, KC_BTN2, KC_WH_U, KC_BTN1, _______, _______,  KC_TILD,    KC_UNDS, KC_PLUS,    KC_LCBR, KC_RCBR, _______, \
-  _______, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______,  G(KC_LEFT), KC_WH_D, KC_WH_U, G(KC_RGHT), KC_PIPE, _______, \
+  _______, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______,  G(KC_LEFT), KC_PGDN, KC_PGUP, G(KC_RGHT), KC_PIPE, _______, \
   KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,  KC_CIRC,    KC_AMPR, KC_ASTR,    KC_LPRN, KC_RPRN, KC_PIPE  \
 ),
 
@@ -99,18 +125,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_ortho_5x12( \
   _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-  KC_ACL2, KC_MUTE, KC_VOLU, _______, _______, _______,       _______, _______, _______, _______, _______, _______, \
-  KC_ACL1, KC_BRID, KC_VOLD, KC_BRIU, _______, _______,       _______, _______, _______, _______, _______,  KC_F12, \
+  KC_ACL2, KC_MUTE, KC_VOLU, _______, _______,  QWERTY,       _______, _______, _______, _______, _______, _______, \
+  KC_ACL1, KC_BRID, KC_VOLD, KC_BRIU, _______,    GAME,       _______, _______, _______, _______, _______,  KC_F12, \
   KC_ACL0,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, \
   _______, _______, _______, MAGIC_TOGGLE_CTL_GUI, _______, MAGIC_TOGGLE_CTL_GUI,       _______, _______, MAGIC_TOGGLE_CTL_GUI,  _______, _______, _______  \
 )
 };
-
-#ifdef AUDIO_ENABLE
-float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
-float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
-#endif
 
 void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
@@ -122,10 +142,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_qwerty);
-        #endif
         persistent_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case GAME:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_GAME);
+      }
+      return false;
+      break;
+    case GAMEMOD:
+      if (record->event.pressed) {
+        layer_on(_GAMEMOD);
+      } else {
+        layer_off(_GAMEMOD);
       }
       return false;
       break;
