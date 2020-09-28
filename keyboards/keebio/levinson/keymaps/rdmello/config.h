873fb0e9e6af73a9f99334e67076d71205859ace
Rylan Dmello <mail@rylan.coffee>
2020-09-27 22:13:10 -0400

    Removing bootmagic and EEPROM persistence

--- keyboards/keebio/levinson/keymaps/rdmello/config.h
+++ keyboards/keebio/levinson/keymaps/rdmello/config.h
@@ -27,19 +27,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 // #define EE_HANDS
 
-// Rylan, trying more MODTAP settings
-#define PERMISSIVE_HOLD
-
 /* Rylan's mousekeys overrides
 * I prefer faster movement of the mouse here, */
 // MOUSEKEY_DELAY defaults to 300, but let's try a quicker one.
 #define MOUSEKEY_DELAY 50
-
 // MOUSEKEY_INTERVAL defaults to 50, 25 should make it a bit smoother.
 #define MOUSEKEY_INTERVAL 35
-
 // This is like constant speed movement.
 #define MOUSEKEY_TIME_TO_MAX 5
-
-//
+// ???
 #define MOUSEKEY_MAX_SPEED 10
+
+// Playing around with tapping term settings (again)
+#define TAPPING_TERM 250 // default: 200
+// #define PERMISSIVE_HOLD // Sends MOD if any other key is tapped within TAPPING_TERM
+// #define IGNORE_MOD_TAP_INTERRUPT // Does something, IDK what.
+// #define TAPPING_FORCE_HOLD // Disables key repeat for MOD(TAP) keys if they're held down after one tap.
+// #define RETRO_TAPPING // Sends the TAP key if nothing else is pressed outside the TAPPING_TERM.
+// place overrides here
--- keyboards/keebio/levinson/keymaps/rdmello/keymap.c
+++ keyboards/keebio/levinson/keymaps/rdmello/keymap.c
@@ -6,48 +6,46 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _COLEMAK 1
-#define _DVORAK 2
-#define _LOWER 3
-#define _RAISE 4
+#define _DVORAK 0
+#define _QWERTY 1
+#define _LOWER 2
+#define _RAISE 3
 #define _ADJUST 16
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
+  DVORAK = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
-  COLEMAK,
-  DVORAK,
+  QWERTY,
 };
 
 /* COMMON ROW
  * ,-------------------------------------------------------------------------------------------.
- * |Adjust|Raise |Lower | Ctrl  | LOWER| GUI(SPC)| |    SPC | RAISE| Alt | GUI |      |      |
+ * |Adjust|Raise |Lower | GUI  | LOWER| CTL(SPC)|     |    SPC | RAISE| Alt |    |      |      |
  * `-------------------------------------------------------------------------------------------'
  */
 #define RYLAN_LAYOUT(...) LAYOUT_ortho_4x12( \
    __VA_ARGS__, \
-   ADJUST, RAISE, LOWER, KC_LCTL, LOWER, LGUI_T(KC_SPC),      KC_SPC, RAISE, KC_LALT, KC_LGUI, _______, _______ \
+   ADJUST, RAISE, LOWER, KC_LGUI, LOWER, LCTL_T(KC_SPC),      KC_SPC, RAISE, KC_LALT, _______, _______, _______ \
 )
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Qwerty
+/* Dvorak
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Tab  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  /   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
+ * `-----------------------------------------------------------------------------------'
  * |                                    COMMON ROW                                     |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = RYLAN_LAYOUT( \
-  KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P, KC_BSPC,   \
-  CTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,   \
-  KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH, KC_SFTENT  \
+[_DVORAK] = RYLAN_LAYOUT( \
+         KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
+  GUI_T(KC_ESC),  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, \
+        KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SFTENT \
 ),
 
 /* Raise
@@ -63,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] =  RYLAN_LAYOUT( \
   KC_TILD,        KC_BTN2, KC_MS_U, KC_BTN1, KC_PGUP, KC_HOME,      KC_GRV, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_DEL, \
-  CTL_T(KC_ESC),  KC_MS_L, KC_MS_D, KC_MS_R, KC_PGDN,  KC_END,     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BSLS, KC_LOCK, \
+  GUI_T(KC_ESC),  KC_MS_L, KC_MS_D, KC_MS_R, KC_PGDN,  KC_END,     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_BSLS, _______, \
   KC_LSFT,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_ENT  \
 ),
 
@@ -73,15 +71,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |M-SRLT|M-SRDN|M-SRRT|      |      |FFXBCK| PGDN | PGUP |FFXFWD|   |  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  ~   |   !  |   @  |   #  |   $  |   %  |  ^   |  &   |   *  |  (   |   )  |   |  |
+ * |      |   !  |   @  |   #  |   $  |   %  |  ^   |  &   |   *  |  (   |   )  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |                                    COMMON ROW                                     |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = RYLAN_LAYOUT( \
   _______, KC_BTN2, KC_WH_U, KC_BTN1, _______, _______,  KC_TILD,    KC_UNDS, KC_PLUS,    KC_LCBR, KC_RCBR, _______, \
-  _______, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______,  G(KC_LBRC), KC_PGDN, KC_PGUP, G(KC_RBRC), KC_PIPE, _______, \
-  KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,  KC_CIRC,    KC_AMPR, KC_ASTR,    KC_LPRN, KC_RPRN, KC_PIPE  \
+   KC_ESC, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______,  KC_HOME,    KC_PGDN, KC_PGUP,     KC_END, KC_PIPE, _______, \
+  KC_LSFT, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,  KC_CIRC,    KC_AMPR, KC_ASTR,    KC_LPRN, KC_RPRN,  KC_ENT  \
 ),
 
 /* Adjust (Lower + Raise)
@@ -92,75 +90,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |M-SPD0|  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      | TGL  | TGL  |      |      |      |      |      |
+ * |                                    COMMON ROW                                     |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] = LAYOUT_ortho_4x12( \
-  KC_ACL2, KC_MUTE, KC_VOLU, _______,  DVORAK, _______,       _______, _______, _______, _______, _______, _______, \
-  KC_ACL1, KC_BRID, KC_VOLD, KC_BRIU,  QWERTY, _______,       _______, _______, _______, _______, _______,  KC_F12, \
-  KC_ACL0,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, \
-  _______, _______, _______, _______, _______, MAGIC_TOGGLE_CTL_GUI,       _______, _______, MAGIC_TOGGLE_CTL_GUI, _______, _______, _______  \
+[_ADJUST] = RYLAN_LAYOUT( \
+  KC_ACL2, KC_MUTE, KC_VOLU, _______,  DVORAK, _______,       _______, _______,     _______, _______, EEPROM_RESET, RESET, \
+  KC_ACL1, KC_BRID, KC_VOLD, KC_BRIU,  QWERTY, _______,       _______, KC_CAPSLOCK, _______, _______, _______,      KC_F12, \
+  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,         KC_F6,   KC_F7,       KC_F8,   KC_F9,  KC_F10,      KC_F11  \
 ),
 
-/* Colemak
+/* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  | Bksp |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |  "   |
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |                                    COMMON ROW                                     |
  * `-----------------------------------------------------------------------------------'
  */
-[_COLEMAK] = RYLAN_LAYOUT( \
-         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
-  CTL_T(KC_ESC),  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT \
+[_QWERTY] = RYLAN_LAYOUT( \
+  KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P, KC_BSPC,   \
+  GUI_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,   \
+  KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_SLSH, KC_SFTENT  \
 ),
-
-/* Dvorak
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  /   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
- * `-----------------------------------------------------------------------------------'
- */
-[_DVORAK] = RYLAN_LAYOUT( \
-         KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
-  CTL_T(KC_ESC),  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, \
-        KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SFTENT \
-)
 };
 
-#ifdef AUDIO_ENABLE
-float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
-float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
-#endif
-
 void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
-    case COLEMAK:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_COLEMAK);
-      }
-      return false;
-      break;
     case DVORAK:
       if (record->event.pressed) {
         persistent_default_layer_set(1UL<<_DVORAK);
+      }
+      return false;
+      break;
+    case QWERTY:
+      if (record->event.pressed) {
+        persistent_default_layer_set(1UL<<_QWERTY);
       }
       return false;
       break;
